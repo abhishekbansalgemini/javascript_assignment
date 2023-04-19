63782c33bf485ca4e5053cc554886fb11567cfc1
Abhishek Bansal <130365962+abhishekbansalgemini@users.noreply.github.com>
2023-04-19 14:18:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
-var table = document.createElement('table');
-// table.border = "1";
-table.style.borderCollapse = "collapse";
+//Self Invoked Function to create table using JS
 
-//creating Caption
+(function () {
+    var table = document.createElement('table');
+    // table.border = "1";
+    table.style.borderCollapse = "collapse";
 
-var caption = document.createElement('caption');
-var cap = document.createTextNode("Student Table");
-caption.appendChild(cap);
-table.appendChild(caption);
+    //creating Caption
 
-
-// Inserting headings of the table
-
-const fields = ["Name", "Age", "DOB", "Email", "Compamy"];
-
-var thead = document.createElement("thead");
-for(let i=0; i<5; i++){
-    let th = document.createElement("th");
-    let h = document.createTextNode(fields[i]);
-    th.appendChild(h);
-    th.style.border = "2px solid black";
-    th.style.textAlign = "center";
-    thead.append(th);
-}
-table.appendChild(thead)
+    var caption = document.createElement('caption');
+    var cap = document.createTextNode("Student Table");
+    caption.appendChild(cap);
+    table.appendChild(caption);
 
 
+    // Inserting headings of the table
 
-//Inserting Body of the Table
+    const fields = ["Name", "Age", "DOB", "Email", "Compamy"];
 
-
-//Creating Array of rows.
-
-const row1 = ["Abhishek Bansal", 21, "13/12/2002", "abhishekbansal@gmail.com", "Gemini Solutions Pvt Ltd"];
-const row2 = ["Ananya Tripathi", 21, "01/12/2002", "ananyatripathi@gmail.com", "Gemini Solutions Pvt Ltd"];
-const row3 = ["Prakhar Saraswat", 21, "05/12/2002", "prakharsaraswat@gmail.com", "Gemini Solutions Pvt Ltd"];
-
-const rows = [row1, row2, row3];
-
-for(let j=0; j<rows.length; j++){
-    var tbody = document.createElement("tbody");
-    for(let i=0; i<rows[0].length; i++){
-        let td = document.createElement("td");
-        let d = document.createTextNode(rows[j][i]);
-        td.style.border = "2px solid black"
-        td.appendChild(d);
-        td.style.padding = '10px';
-        td.style.textAlign = "center";
-        tbody.append(td);
+    var thead = document.createElement("thead");
+    for (let i = 0; i < 5; i++) {
+        let th = document.createElement("th");
+        let h = document.createTextNode(fields[i]);
+        th.appendChild(h);
+        th.style.border = "2px solid black";
+        th.style.textAlign = "center";
+        thead.append(th);
     }
-    table.appendChild(tbody);
-}
+    table.appendChild(thead)
 
 
-document.body.appendChild(table);
 
 
+
+    //Inserting Body of the Table
+
+
+
+    //Creating Array of rows.
+
+    const row1 = ["Abhishek Bansal", 21, "13/12/2002", "abhishekbansal@gmail.com", "Gemini Solutions Pvt Ltd"];
+    const row2 = ["Ananya Tripathi", 21, "01/12/2002", "ananyatripathi@gmail.com", "Gemini Solutions Pvt Ltd"];
+    const row3 = ["Prakhar Saraswat", 21, "05/12/2002", "prakharsaraswat@gmail.com", "Gemini Solutions Pvt Ltd"];
+
+    const rows = [row1, row2, row3];
+
+    for (let j = 0; j < rows.length; j++) {
+        var tbody = document.createElement("tbody");
+        for (let i = 0; i < rows[0].length; i++) {
+            let td = document.createElement("td");
+            let d = document.createTextNode(rows[j][i]);
+            td.style.border = "2px solid black"
+            td.appendChild(d);
+            td.style.padding = '10px';
+            td.style.textAlign = "center";
+            tbody.append(td);
+        }
+        table.appendChild(tbody);
+    }
+
+
+    document.body.appendChild(table);
+
+
+
+})();
